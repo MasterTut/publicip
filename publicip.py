c12a364affddd408f08c8642e8c48c6897ddb351
veryangrymonk <mtut@protonmail.com>
2020-06-20 12:18:24 -0500

    Json not read API

--- publicip.py
+++ publicip.py
@@ -1,7 +1,41 @@
 import requests
 import pygame
+from pygame.locals import *
+import json
+pygame.init()
+
+
+
+#get public IP
 def getip():
     response = requests.get('https://api.ipify.org?format=json')
-    print(response.json())
+    results = response.json()
+    return json.load(results)
+
+
+#create window
+def drawwindow():
+    pygame.display.set_caption('Public IP')
+    surface = pygame.display.set_mode((200, 100))
+    myfont = pygame.font.SysFont('Droid Sans', 30)
+    textsurface = myfont.render(getip(), False, (255, 0, 0))
+    surface.blit(textsurface,(0,0))
+    pygame.display.flip()
+
+
+# Game Loop
+def gameloop():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        drawwindow()
+
+
+
+
+
+
 
 getip()
